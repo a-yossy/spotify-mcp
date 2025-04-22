5ebe0200151a2705eef954f6d55ef2a5a70a7689
a-yossy <atsuya0930@yahoo.co.jp>
2025-04-23 01:12:49 +0900

    Add UpsertInput struct and implement upsert method for MusicSearchProgress

--- src/model/music_search_progress.rs
+++ src/model/music_search_progress.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use chrono::NaiveDateTime;
+use sqlx::MySqlPool;
 
 pub struct MusicSearchProgress {
     pub id: u32,
@@ -9,9 +10,19 @@ pub struct MusicSearchProgress {
     pub updated_at: NaiveDateTime,
 }
 
+pub struct UpsertInput {
+    pub position: u32,
+}
+
+impl UpsertInput {
+    pub fn new(position: u32) -> Self {
+        Self { position }
+    }
+}
+
 impl MusicSearchProgress {
     pub async fn find_by_music_genre_id(
-        db_pool: &sqlx::MySqlPool,
+        db_pool: &MySqlPool,
         music_genre_id: u32,
     ) -> Result<Option<Self>> {
         let result = sqlx::query_as!(
@@ -30,5 +41,45 @@ impl MusicSearchProgress {
         .await?;
 
         Ok(result)
+    }
+
+    pub async fn upsert(
+        db_pool: &MySqlPool,
+        music_genre_id: u32,
+        input: &UpsertInput,
+    ) -> Result<Self> {
+        let id = sqlx::query!(
+            r#"
+                INSERT INTO
+                  music_search_progresses (music_genre_id, position)
+                VALUES
+                  (?, ?)
+                ON DUPLICATE KEY UPDATE
+                  position = ?
+            "#,
+            music_genre_id,
+            input.position,
+            input.position
+        )
+        .execute(db_pool)
+        .await?
+        .last_insert_id();
+
+        let music_search_progress = sqlx::query_as!(
+            MusicSearchProgress,
+            r#"
+                SELECT
+                  id, music_genre_id, position, created_at, updated_at
+                FROM
+                  music_search_progresses
+                WHERE
+                  id = ?
+            "#,
+            id
+        )
+        .fetch_one(db_pool)
+        .await?;
+
+        Ok(music_search_progress)
     }
 }
