e07340270a1ac8b70c2f7c7a331192e6f179a93a
a-yossy <atsuya0930@yahoo.co.jp>
2025-04-29 17:08:29 +0900

    Improve error handling in dotenv loading process in build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,29 @@
-use dotenvy::{dotenv, dotenv_iter};
+use dotenvy::{Error, dotenv, dotenv_iter};
+
+fn env_error(e: Error) {
+    eprintln!("環境変数の読み込みに失敗しました: {}", e);
+    std::process::exit(1);
+}
 
 fn main() {
-    dotenv().ok();
-    for item in dotenv_iter().unwrap() {
-        let (key, val) = item.unwrap();
-        println!("cargo:rustc-env={}={}", key, val);
+    match dotenv() {
+        Ok(_) => {}
+        Err(e) => {
+            env_error(e);
+        }
+    }
+
+    for item in dotenv_iter().unwrap_or_else(|e| {
+        env_error(e);
+        unreachable!();
+    }) {
+        match item {
+            Ok((key, val)) => {
+                println!("cargo:rustc-env={}={}", key, val);
+            }
+            Err(e) => {
+                env_error(e);
+            }
+        }
     }
 }
